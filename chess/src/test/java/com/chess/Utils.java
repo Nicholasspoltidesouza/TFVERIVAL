23d7d28dc66263c6e5cc093c70f93b8ea215c930
Nicholas Spolti De Souza <nicholasspoltidesouza@gmail.com>
2023-06-06 16:37:54 -0300

    WIP

---
+++ chess/src/test/java/com/chess/Utils.java
@@ -0,0 +1,25 @@
+package com.chess;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Utils {
+
+    protected static WebDriver driver;
+
+    public static WebDriver browser(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+}
